0393710c3eac5b58d9f63b1cc316dca5904a2616
jb <j3b.dev@gmail.com>
2023-03-28 12:02:28 +0200

    user-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Pour utiliser ngModel, c'ets le modulue des formulaires
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { MonComposantComponent } from './mon-composant/mon-composant.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonComposantComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mon-composant/mon-composant.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-composant',
+  templateUrl: './mon-composant.component.html',
+  styleUrls: ['./mon-composant.component.css']
+})
+export class MonComposantComponent implements OnInit {
+  name : string = "Jb";
+  imageSrc: string = "https://via.placeholder.com/350x150";
+  constructor() {}
+  ngOnInit(): void{}
+  sayHello(): void  {
+    alert("Bonjour "+ this.name + "!");
+  }
+}
